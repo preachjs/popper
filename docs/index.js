3f0995f53edc73664939044f84ff0dd935772bfe
reaper <ahoy@barelyhuman.dev>
2025-01-03 21:54:26 +0530

    fix: add resize handler to demo

--- docs/index.js
+++ docs/index.js
@@ -6,22 +6,43 @@ const popper = createPopper(anchor, target)
 
 const positionButtons = document.querySelectorAll('[data-position]')
 const alignedButtons = document.querySelectorAll('[data-align]')
+const offsetRange = document.querySelector('#offsetRange')
 
 let currentPosition = 'bottom'
 let currentAlign = 'center'
 
+window.addEventListener('resize', () => {
+  popper
+    .move(currentPosition, currentAlign)
+    .offset(Number(offsetRange.value))
+    .align()
+})
+
+offsetRange.addEventListener('change', e => {
+  popper.offset(Number(e.target.value)).align()
+})
+
 positionButtons.forEach(btn => {
   btn.addEventListener('click', () => {
-    popper.move(btn.dataset.position, currentAlign).align()
+    popper
+      .move(btn.dataset.position, currentAlign)
+      .offset(Number(offsetRange.value))
+      .align()
     currentPosition = btn.dataset.position
   })
 })
 
 alignedButtons.forEach(btn => {
   btn.addEventListener('click', () => {
-    popper.move(currentPosition, btn.dataset.align).align()
+    popper
+      .move(currentPosition, btn.dataset.align)
+      .offset(Number(offsetRange.value))
+      .align()
     currentAlign = btn.dataset.align
   })
 })
 
-popper.move(currentPosition, currentAlign).offset(10).align()
+popper
+  .move(currentPosition, currentAlign)
+  .offset(Number(offsetRange.value))
+  .align()
