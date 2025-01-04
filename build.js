7c2e602876beefc012d2c43ee3db506185dea634
reaper <ahoy@barelyhuman.dev>
2025-01-04 12:06:33 +0530

    chore: build tooling

--- build.js
+++ build.js
@@ -1,28 +1,82 @@
-import { build } from 'esbuild'
+import path, { join } from 'node:path'
+import { parseArgs } from 'node:util'
+import chokidar from 'chokidar'
+import { analyzeMetafile, context } from 'esbuild'
 import dtsPkg from 'npm-dts'
-
 const { Generator } = dtsPkg
 
-await build({
-  entryPoints: ['./src/index.ts'],
-  bundle: true,
-  outdir: 'dist',
-  format: 'esm',
+const flag = parseArgs({
+  options: {
+    watch: {
+      type: 'boolean',
+      short: 'w',
+      default: false,
+    },
+  },
 })
 
-await new Generator({
-  entry: 'src/index.ts',
-  output: 'dist/index.d.ts',
-}).generate()
+const { watch } = flag.values
+await createBuilder('src/index.ts', 'dist')
+await createBuilder('src/index.ts', 'docs/bundle', 'docs')
 
-await build({
-  entryPoints: ['./src/index.ts'],
-  bundle: true,
-  outdir: 'docs/bundle',
-  format: 'esm',
-})
+async function createBuilder(input, output, serveDir) {
+  const watchGraph = []
+  const ctx = await context({
+    entryPoints: [input],
+    bundle: true,
+    outdir: output,
+    format: 'esm',
+    metafile: true,
+    plugins: [
+      {
+        name: 'watch-graph',
+        setup(builder) {
+          builder.onResolve({ filter: /.*/ }, args => {
+            watchGraph.push(path.resolve(args.path))
+            return
+          })
+        },
+      },
+    ],
+  })
 
-await new Generator({
-  entry: 'src/index.ts',
-  output: 'docs/bundle/index.d.ts',
-}).generate()
+  async function generateBundle() {
+    const buildOut = await ctx.rebuild()
+    console.log(await analyzeMetafile(buildOut.metafile))
+    await new Generator({
+      entry: input,
+      output: path.join(output, 'index.d.ts'),
+    }).generate()
+  }
+
+  if (watch) {
+    const watcher = chokidar.watch(input, {
+      ignored(f) {
+        return f.startsWith('node_modules') || f.startsWith(path.join(output))
+      },
+    })
+    const usableGraph = watchGraph.filter(
+      d => Boolean(d) && join(d) != join(input)
+    )
+    if (usableGraph.length) {
+      watcher.add(...usableGraph)
+    }
+    watcher.on('all', async (e, f) => {
+      if (e === 'change') {
+        console.log(`${f} changed, rebuilding...`)
+      }
+      await generateBundle()
+    })
+  }
+
+  await generateBundle()
+  if (!watch) {
+    await ctx.dispose()
+  }
+
+  if (watch && serveDir) {
+    await ctx.serve({
+      servedir: serveDir,
+    })
+  }
+}
